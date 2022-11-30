9deda6d0371e93d734b723df164de2306a6b4479
Julia <wuwara@mail.ru>
2022-11-30 14:42:09 +0300

    chenged the solution of the second task

--- Program.cs
+++ Program.cs
@@ -16,25 +16,53 @@
 //    Console.Write("min = " + number1); 
 // }
 
-
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
+// Второй вариант с учётом, если введут одинаковые числа
 
 // Console.Write("Enter first number: ");
 // int number1 = Convert.ToInt32(Console.ReadLine());
 // Console.Write("Enter socond number: ");
-// int number2  = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Enter third number: ");
-// int number3  = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 > number2)
+// {
+//     Console.WriteLine("max = " + number1);
+//     Console.WriteLine("min = " + number2);
+// }
+// else if (number2 > number1)
+// {
+//    Console.WriteLine("max = " + number2);
+//    Console.Write("min = " + number1); 
+// }
+// else 
+//     Console.WriteLine ("Error. Enter different numbers, pleas.");
 
-// int max = number1;
 
-// if ( number1 > max) max = number1;
-// if (number2 > max) max = number2;
-// if (number3 > max) max = number3;
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-// Console.Write("Max = ");
-// Console.WriteLine(max);
+
+Console.Write("Enter first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter socond number: ");
+int number2  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+int number3  = Convert.ToInt32(Console.ReadLine());
+
+if ( number1 > number2 && number1 > number3) 
+{
+    Console.WriteLine("max = " + number1);
+}
+else if (number2 > number1 && number2 > number3) 
+{
+    Console.WriteLine("max = " + number2);
+}
+else if (number3 > number1 && number3 > number2)
+{
+    Console.WriteLine("max = " + number3);
+}
+else
+{
+    Console.WriteLine("Error. Pleas, try to enter different numbers");
+}
+
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
